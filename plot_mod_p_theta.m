5d91505d6236ede870d5a3acf832c57142d46268
kpiyush25 <piyushk@iitk.ac.in>
2022-09-26 15:40:39 +0530

    Add function to plot |p_theta|

---
+++ plot_mod_p_theta.m
@@ -0,0 +1,12 @@
+function plot_mod_p_theta(mod_p_theta, M)
+    endpoint = 1500;
+    time = linspace(0, endpoint, endpoint);
+    for i = 1:M
+        plot(time, mod_p_theta(1:endpoint,i));
+        hold on;
+    end  
+    title('Plot of mod of p_{theta} vs number of iterations(time)');
+    xlabel('Number of iterations');
+    ylabel('|p_{theta}|');
+    legend('|p_{1theta}|','|p_{2theta}|','|p_{3theta}|','|p_{4theta}|','|p_{5theta}|','|p_{6theta}|');
+end
