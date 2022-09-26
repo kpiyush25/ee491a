52735ed7217bdf42b79e69792a2f619d94e056fd
kpiyush25 <piyushk@iitk.ac.in>
2022-09-26 15:39:29 +0530

    Implement the logic using 3D matrix and not classes

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 clc; clear all; close all;
-num_iterations = 100;
+num_iterations = 2000;
 M = 6; % M and N correspond to M-N phase patterns
 N = 6;
 delta = 5.5;
@@ -16,13 +16,13 @@ K(6) = -0.05;
 
 p_theta = zeros(1, M-1);
 
-% system_model = system_model();
 X_0 = [-1; 0.5; 2*sqrt(2); 5*sqrt(3); 15; 3*sqrt(3)];
 Y_0 = [-3*sqrt(3); -15; -3*sqrt(2); 0; 0.5; 9];
-Theta_0 = [-pi/6; 0; pi/4; pi/3; pi/2; 2*pi/3];
+Theta_0 = [-pi/6; pi/15; pi/4; pi/3; pi/2; 2*pi/3];
 theta = zeros(num_iterations, N);
 errors = zeros(num_iterations, N);
 p_theta = zeros(num_iterations, M);
+agents = zeros(num_iterations, N, 5);
 for i = 1:N
     theta(1, i) = Theta_0(i);
 end
@@ -31,43 +31,59 @@ for m = 1:M-1
     p_theta(1, m) = compute_p_m_theta(m, N, theta(1,:));
 end
 p_theta(1, M) = 1/M;
-for i =1:N
-    agents(1, i) = agent(X_0(i), Y_0(i), Theta_0(i), 0);
+for k = 1:num_iterations
+    for i =1:N
+        agents(k, i, :) = create_agent(X_0(i), Y_0(i), Theta_0(i), 0);
+    end
 end
 for i = 1:N
-    errors(1, i) = (agents(1, i).r - c_d) + 1i*(1/omega_d)*exp(1i*agents(1,i).theta);
+    errors(1, i) = (agents(1, i, 3) - c_d) + 1i*(1/omega_d)*exp(1i*agents(1,i,4));
+    
+    
 end
 for i = 1:N
-    agents(1, i).theta_dot = compute_u_k(omega_d, kappa, delta, c_d, K, M, agents(1, i).r, agents(1, i).theta, p_theta(1,:), errors(1,i));
+    agents(1, i, 5) = compute_u_k(omega_d, kappa, delta, c_d, K, M, agents(1, i, 3), agents(1, i, 4), p_theta(1,:), errors(1,i));
+    % fprintf("u_k for agent %d : %d\n",i, agents(1,i,5));
 end
-disp(agents(1,1).theta);
-disp(agents(1,1).theta_dot);
-agent(1,1) = agent(0,0,0,0);
+
 for k = 2:num_iterations
     for i = 1:N
-%         agents(k, i) = agent(agents(k-1,i).x, agents(k-1,i).y, agents(k-1,i).theta + agents(k-1,i).theta_dot, 0);
-        agents(k, i) = agent(0,0,0,0);
+        agents(k, i, 4) = agents(k-1,i,4) + agents(k-1,i, 5);
+        % tsp = [0 600];
+        % [t, agents(k, i, 4)] = ode45(@(t, agents(k, i, 4)), )
+        agents(k, i, 3) = agents(k-1, i, 3) + exp(1i*agents(k-1, i, 4));
+    end
+    for i = 1:N
+        errors(k, i) = (agents(k, i, 3) - c_d) + 1i*(1/omega_d)*exp(1i*agents(k,i,4));
     end
     for i = 1:M-1
-        p_theta(k,i) = compute_p_m_theta(m, N, agents(k,:).theta);
+        p_theta(k,i) = compute_p_m_theta(m, N, agents(k,:,4));
     end
     p_theta(k, M) = 1/M;
     for i = 1:N
-        %update x, y, r here:
-        agents(k, i).r = agents(k-1, i).r + exp(1i*agents(k, i).theta);
-        agents(k, i).x = real(agents(k, i).r);
-        agents(k, i).y = imag(agents(k, i).r);
-        errors(k, i) = (agents(k, i).r - c_d) + 1i*(1/omega_d)*exp(1i*agents(k,i).theta);
-        agents(k, i).theta_dot = compute_u_k(omega_d, kappa, delta, c_d, K, M, agents(k, i).r, agents(k, i).theta, p_theta(k), errors(k));
+        agents(k, i, 1) = real(agents(k, i, 3));
+        agents(k, i, 2) = imag(agents(k, i, 3));
+        errors(k, i) = (agents(k, i, 3) - c_d) + 1i*(1/omega_d)*exp(1i*agents(k,i,4));
+        agents(k, i, 5) = compute_u_k(omega_d, kappa, delta, c_d, K, M, agents(k, i, 3), agents(k, i, 4), p_theta(k,:), errors(k,i));
+%         fprintf("u_k for agent %d in iteration %d : %d\n",i,k, agents(k,i,5));
+        disp(agents(k,i,5));
     end
 end
 
-for i = 1:N
-%     disp(agents(i, 1).x);
-%     disp(agents(i, 1).y);
+mod_p_theta = zeros(num_iterations, M);
+for i = 1:num_iterations
+    for j = 1:M
+        mod_p_theta(i, j) = abs(p_theta(i, j));
+    end
 end
 
-plot(agents(1).x, agents(1).y); 
-    
+mod_errors = zeros(num_iterations, N);
+for i = 1:num_iterations
+    for j = 1:N
+        mod_errors(i, j) = abs(errors(i, j));
+    end
+end
+plots(N, M, agents(:,:,:), mod_p_theta(:,:), mod_errors(:,:));
+toc
     
     
