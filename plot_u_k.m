044b34e38fcf4d2649260b8955ee71c602ad1b67
kpiyush25 <piyushk@iitk.ac.in>
2022-09-26 15:41:20 +0530

    Add function to plot u_k

---
+++ plot_u_k.m
@@ -0,0 +1,12 @@
+function plot_u_k(agents, N)
+    endpoint = 1998;
+    time = linspace(0, endpoint, endpoint);
+    for i = 1:N
+        plot(time, agents(1:endpoint,i,5));
+        hold on;
+    end 
+    title('Plot of u_k vs number of iterations(time)');
+    xlabel('Number of iterations(time)');
+    ylabel('u_k');
+    legend('u_1','u_2','u_3','u_4','u_5','u_6');
+end
