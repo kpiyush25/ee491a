c443b975f2de42871191b011c64449ebe14c1176
kpiyush25 <piyushk@iitk.ac.in>
2022-10-18 04:42:49 +0530

    Use our functions for simulation

---
+++ simulation_using_ode45/integrating_function2.m
@@ -0,0 +1,47 @@
+function dXdt = integrating_function(t, X)
+    % Constants:
+    omega_d = 0.1;
+    omega_d_inv = 1/omega_d;
+    kappa = 0.005;
+    c_d = 0 + 1i*0;
+    delta = 5;
+    K = zeros(6, 1);
+    for i = 1:5
+        K(i) = 0.05;
+    end
+    K(6) = -0.05;
+    N = 6;
+    M = 6;
+
+    % Variables : 
+    p_theta = zeros(M, 1);
+    for m = 1 : M-1
+        for k = 1 : N
+            p_theta(m,1) = p_theta(m,1) + exp(1i*m*X(3*(k-1)+1,1));
+        end
+        p_theta(m,1) = p_theta(m,1)/(m*N);
+    end
+    p_theta(M,1) = 1/M;
+
+    dXdt = zeros(N*3,1);
+    for k = 1:N
+        e_k = X(3*(k-1)+2, 1) + 1i*X(3*k, 1) - c_d + 1i*omega_d_inv*exp(1i*X(3*(k-1)+1, 1));
+        
+         %  The code below is our proposition similar to equation (20) to solve the problem of the paper:
+        % dXdt(3*(k-1)+1, 1) = omega_d + (2*(delta^2)*kappa*real(conj(X(3*(k-1)+2, 1) + 1i*X(3*k, 1) - c_d)*exp((1i*X(3*(k-1)+1, 1)))))/(omega_d*((delta^2 - (abs(e_k))^2)^2));
+        
+        % The code below is to avoid the inner circle:
+        % u_k = omega_d + (2*((abs(e_k)^4) - delta^4)*kappa*real(conj(r_k - c_d)*exp((1i*theta_k))))/(omega_d*((abs(e_k))^4));
+        % dXdt(3*(k-1)+1, 1) = omega_d + (2*((abs(e_k)^4) - delta^4)*kappa*real(conj(X(3*(k-1)+2, 1) + 1i*X(3*k, 1) - c_d)*exp((1i*X(3*(k-1)+1, 1)))))/(omega_d*((abs(e_k))^4));
+        
+        % For experimental barrier lyapunov functions
+        dXdt(3*(k-1)+1, 1) = omega_d + (2*((abs(e_k)^4) - delta^4)*kappa*real(conj(X(3*(k-1)+2, 1) + 1i*X(3*k, 1) - c_d)*exp((1i*X(3*(k-1)+1, 1)))))/(omega_d*((abs(e_k))^4));
+
+        for m = 1 : M
+            dXdt(3*(k-1)+1, 1) = dXdt(3*(k-1)+1, 1) - K(m)*real(conj(p_theta(m,1))*1i*exp(1i*m*X(3*(k-1)+1, 1)));
+        end
+
+        dXdt(3*(k-1)+2, 1) = cos(X(3*(k-1)+1, 1));
+        dXdt(3*(k-1)+3, 1) = sin(X(3*(k-1)+1, 1));
+    end
+end
---
+++ simulation_using_ode45/main2.m
@@ -0,0 +1,40 @@
+clc; clear all; close all;
+tic;
+N = 6;
+M = 6;
+omega_d = 0.1;
+omega_d_inv = 1/omega_d;
+c_d = 0 + 1i*0;
+
+% X_0 = [-1; 0.5; 2*sqrt(2); 5*sqrt(3); 15; 3*sqrt(3)];
+% Y_0 = [-3*sqrt(3); -14; -3*sqrt(2); 0; 0.5; 9];
+% Theta_0 = [-pi/6; 0; pi/4; pi/3; pi/2; 2*pi/3];
+
+X_0 = [-10; 10.5; 12*sqrt(2); 15*sqrt(3); 15; 13*sqrt(3)];
+Y_0 = [-13*sqrt(3); -14; -13*sqrt(2); 10; 10.5; 9];
+Theta_0 = [-pi/6; 0; pi/4; pi/3; pi/2; 2*pi/3];
+
+t_start = 0;
+t_end = 600;
+% tspan = [t_start : 0.1 : t_end];
+tspan = [t_start t_end]
+
+X_init = zeros(N*3,1);
+for k = 1:N
+    X_init((k-1)*3 + 1, 1) = Theta_0(k);
+    X_init((k-1)*3 + 2, 1) = X_0(k);
+    X_init((k-1)*3 + 3, 1) = Y_0(k);
+end
+
+options=odeset('AbsTol',1e-12,'RelTol',1e-12);
+[t, X] = ode45(@integrating_function2, tspan, X_init,options);
+size(X)
+len = size(X, 1)
+dXdt = zeros(len, 3*N);
+for i = 1 : len
+    dXdt(i, :) = integrating_function2(t(i), X(i, :)');
+end
+
+plots(t, X, dXdt, len, omega_d_inv, M, N, c_d);
+
+toc
